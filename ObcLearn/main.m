046879452dbffff46bfb24fff9dc39f63f91d7a9
haiMac <mac@haimac.local>
2016-02-15 07:16:51 +0800

    Initial Commit

---
+++ ObcLearn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObcLearn
+//
+//  Created by haiMac on 16/2/15.
+//  Copyright © 2016年 haiMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
